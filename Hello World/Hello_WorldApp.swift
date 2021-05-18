502f0fa43c7126b47874291caeb2c41beee79d4e
Jiulong Shan <jiulong.shan@gmail.com>
2021-05-18 16:48:57 +0800

    first commit

---
+++ Hello World/Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldApp.swift
+//  Hello World
+//
+//  Created by Jiulong Shan on 5/14/21.
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
